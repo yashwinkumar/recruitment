19373698585b8c4e3507195606064bb8841deadf
= <=>
2016-02-23 11:11:48 +0530

    commented for heroku migration

--- db/migrate/20160204180951_add_status_to_submission.rb
+++ db/migrate/20160204180951_add_status_to_submission.rb
@@ -2,5 +2,5 @@ class AddStatusToSubmission < ActiveRecord::Migration
   def change
     add_column :submissions, :status, :string
   end
-  Submission.update_all(status:"submitted")
+   # Submission.update_all(status:"submitted")
 end
